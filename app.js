974d9963b3e8c55862eef69759f14bab3c5408b0
Lucas <xolmirzayevotabek779@gmail.com>
2023-05-16 20:41:08 +0900

    feat: FrontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const res = require("express/lib/response");
 const app = express();
 const fs = require("fs");
 
-
 // MongoDB connect/ chaqirish
 const db = require("./server").db();
 
@@ -42,13 +41,16 @@ app.post("/create-item", (req, res) => {
   // res.end("success");
   console.log(req.body);
   const new_reja = req.body.reja;
-  db.collection('plans').insertOne({reja: new_reja}, (err, data) => {
-    if(err) {
-      console.log(err);
-      res.end('something went wrong');
-    } else {
-      res.end("successfully added");
-    }
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    console.log(data.ops);
+    res.json(data.ops[0]);
+
+    // if(err) {
+    //   console.log(err);
+    //   res.end('something went wrong');
+    // } else {
+    //   res.end("successfully added");
+    // }
   });
 });
 
@@ -57,22 +59,19 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", function (req, res) {
-
-  console.log('user entered /');
+  console.log("user entered /");
   db.collection("plans")
-  .find()
-  .toArray((err, data) => {
-    if(err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      // console.log(data);
-      res.render("reja", {items: data});
-      // res.render("reja");
-    }
-  })
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        // console.log(data);
+        res.render("reja", { items: data });
+        // res.render("reja");
+      }
+    });
 });
 
-
 module.exports = app;
-
